3517ecc6f677a207dc3ef783a31a6b4af99b1e2d
sf-yuzifu <yuzifu@yuzifu.top>
2024-08-11 12:19:29 +0800

    [feat]鼠标特效

--- src/components/Cursor.vue
+++ src/components/Cursor.vue
@@ -23,9 +23,26 @@ document.body.onmouseenter = function () {
 document.body.onclick = function () {
   let a = document.createElement('div')
   a.classList.add('click')
-  a.style.left = posX.value - 75 / 2 + 'px'
-  a.style.top = posY.value - 75 / 2 + 'px'
+  a.style.left = posX.value - 50 / 2 + 'px'
+  a.style.top = posY.value - 50 / 2 + 'px'
+
+  let b = document.createElement('div')
+  let c = Math.random() * 360
+  b.style.transform = 'rotate(' + c + 'deg)'
+  b.classList.add('inner')
+  a.appendChild(b)
+
+  let d = document.createElement('div')
+  let e = Math.random() * 360
+  d.style.transform = 'rotate(' + e + 'deg)'
+  d.classList.add('inner')
+  a.appendChild(d)
+
   document.body.appendChild(a)
+  setTimeout(() => {
+    b.style.transform = 'rotate(' + (c + 180) + 'deg)'
+    d.style.transform = 'rotate(' + (e + 180) + 'deg)'
+  }, 1)
   setTimeout(() => {
     a.remove()
   }, 500)
@@ -70,43 +87,73 @@ window.document.body.onmouseover = function (event) {
 }
 
 .click {
-  width: 75px;
-  height: 75px;
+  width: 50px;
+  height: 50px;
   border-radius: 100%;
   background: #c3ebff;
   position: fixed;
   left: 0;
   top: 0;
+  display: grid;
   transform: scale(0%);
   border: 3px solid transparent;
   filter: drop-shadow(0px 0px 6px #c3ebff);
-  animation: move 0.5s ease-out;
+  animation: move 0.45s ease-out;
+  pointer-events: none;
+  align-items: center;
+  justify-items: center;
+}
+
+.click .inner {
+  width: calc(100% + 1px);
+  height: calc(100% + 1px);
+  border-top: 3px solid transparent;
+  border-right: 3px solid transparent;
+  border-left: 3px solid transparent;
+  border-bottom: 3px solid transparent;
+  border-radius: 100%;
+  position: absolute;
+  transition: transform 0.5s linear;
+  animation: move2 0.5s ease-out;
+}
+
+@keyframes move2 {
+  0% {
+    border-top: 3px solid #77deff;
+  }
+  25% {
+    border-top: 3px solid #fff;
+  }
+  50% {
+    border-top: 3px solid #fff;
+  }
+  80% {
+    border-top: 3px solid transparent;
+  }
+  100% {
+    border-top: 3px solid transparent;
+  }
 }
 
 @keyframes move {
   0% {
     transform: scale(0%);
-    border: 3px solid #77deff;
     background: #c3ebff;
   }
   25% {
     transform: scale(100%);
-    border: 3px solid #fff;
     background: #c3ebff66;
   }
   50% {
     transform: scale(100%);
-    border: 3px solid #fff;
     background: transparent;
   }
   80% {
     transform: scale(100%);
-    border: 3px solid transparent;
     background: transparent;
   }
   100% {
     transform: scale(100%);
-    border: 3px solid transparent;
     background: transparent;
   }
 }
